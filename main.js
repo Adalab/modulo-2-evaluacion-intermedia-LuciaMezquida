19c2f14cf5f9de51dfb69cf0e405c100d4df631a
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-13 10:00:06 +0200

    Create counter

--- main.js
+++ main.js
@@ -1,32 +1,39 @@
 'use strict';
 
+const gameBoard = document.querySelector('.js-game');
 const number = document.querySelector('.js-number');
 const button = document.querySelector('.js-button');
 const hint = document.querySelector('.js-hint');
 const counter = document.querySelector('.js-counter');
 
+
+
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 const numberToGuess = getRandomNumber(100)
-console.log(numberToGuess)
+let attempsNumber = 0;
+console.log('Mi número aleatorio es: ' + numberToGuess)
 
 function checkNumber(){
   const numberValue = parseInt(number.value);
-  let attempsNumber = 0;
+  console.log('El número introducido es: ' + numberValue)
   if (numberValue < 1 || numberValue > 100) {
-    hint.innerHTML = 'El número debe estar entre 1 y 100';
+    hint.value = 'El número debe estar entre 1 y 100';
     attempsNumber += 1;
-    
+    counter.value = 'Número de intentos: ' + attempsNumber;
   }else if(numberValue > numberToGuess) {
-    hint.innerHTML = 'Demasiado alto';
+    hint.value = 'Demasiado alto';
     attempsNumber += 1;
+    counter.value = 'Número de intentos: ' + attempsNumber;
   } else if (numberValue < numberToGuess) {
-    hint.innerHTML = 'Demasiado bajo';
+    hint.value = 'Demasiado bajo';
     attempsNumber += 1;
+    counter.value = 'Número de intentos: ' + attempsNumber;
   } else if (numberValue === numberToGuess) {
-    hint.innerHTML = 'Has ganado Campeona!!!';
+    hint.value = 'Has ganado Campeona!!!';
     attempsNumber += 1;
+    counter.value = 'Número de intentos: ' + attempsNumber;
   }
 } 
 
